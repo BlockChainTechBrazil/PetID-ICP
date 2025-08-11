ba5ddabcf49edb4f3c46f2dbc9852d266a7fdc67
davimqz <dmo3@cesar.school>
2025-08-10 22:38:42 -0300

    [Feat] - Favicon nd logo

--- src/PetID_frontend/src/componentes/NavBar.jsx
+++ src/PetID_frontend/src/componentes/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
-
+import Logo from "../../dist/assets/logo/petid_logo_png.png"
 const NavBar = () => {
   const { t } = useTranslation();
   
@@ -10,14 +10,11 @@ const NavBar = () => {
       <div className="container px-4 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-            <img src="/logo2.svg" alt="PetID Logo" className="h-10 w-10 mr-2" />
+            <img src={Logo} alt="PetID Logo" className="h-10 w-10 mr-2" />
             <span className="text-2xl font-bold tracking-tight text-blue-600">PetID</span>
           </div>
           <div className="flex items-center gap-3">
             <LanguageSwitcher />
-            <button className="px-5 py-2 rounded-full bg-blue-500 hover:bg-blue-600 text-white font-semibold shadow-lg transition-all duration-200">
-              {t('navbar.connectWallet')}
-            </button>
           </div>
         </div>
       </div>
