848da78296731222acf5e017e70d7fb0d378aad8
Alex <joubert2006@hotmail.com>
2025-08-20 19:00:05 -0300

    fix: run aplication

--- src/PetID_frontend/dev-dist/sw.js
+++ src/PetID_frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-29abfca6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.l8qhutehvag"
+    "revision": "0.f0o7igjggfk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
